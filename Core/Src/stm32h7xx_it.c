e36a262381b9dba5ec853016618df7ffaf6d16ff
ExMikuPro <hatsunemiku@HatsunedeMacBook-Pro.local>
2026-02-06 13:55:39 +0800

    添加DMA2D+VSync+Launcher部分界面

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern LTDC_HandleTypeDef hltdc;
 extern MDMA_HandleTypeDef hmdma_mdma_channel0_sw_0;
 /* USER CODE BEGIN EV */
 
@@ -204,6 +205,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32h7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles LTDC global interrupt.
+  */
+void LTDC_IRQHandler(void)
+{
+  /* USER CODE BEGIN LTDC_IRQn 0 */
+
+  /* USER CODE END LTDC_IRQn 0 */
+  HAL_LTDC_IRQHandler(&hltdc);
+  /* USER CODE BEGIN LTDC_IRQn 1 */
+
+  /* USER CODE END LTDC_IRQn 1 */
+}
 
 /**
   * @brief This function handles MDMA global interrupt.
